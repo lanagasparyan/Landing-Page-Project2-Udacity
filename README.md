30ec0e9a75e94919fb3a689970736a14a83aa66a
Lana Gasparyan <lana5545@gmail.com>
2020-03-14 17:19:51 -0400

    Changes in JS and README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Landing Page Project
+Udacity's Front End Nanodegree
+Starter code taken from udacity [repository](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page).
 
 ## Table of Contents
 
@@ -11,3 +13,18 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+## Thoughts
+
+This project improved my thinking and helped me to imagine how code works and what it does in each section. How active class adds and removes to HTML in realtime to an element and makes it interactive. 
+Before this project I used to keep editing within the same code making it messy to read and understand later, now I start with trying first my approach on a smaller scale and then moving to the real project.
+It is a good experience of understanding that the best way of making a project is to write the plan or logic on a paper and then all left is to translate it into a JavaScript or any other language.
+
+## Description
+
+This is a  landing page that uses Javascript to create a responsive  NavBar and sections onscroll. HTML, CSS and JS is used.
+
+### To view project
+
+The project is published on GitHub Pages. To view, click on this link: [link to project](https://github.com/lanagasparyan?tab=repositories).
--- js/app.js
+++ js/app.js
@@ -10,20 +10,20 @@ function createNav() {
    
     For loop is 
     */
-    const navItems = ['Section1', 'Section2', 'Section3', 'Section4', 'Home'];
+    const navItems = ['News', 'Life', 'Comments', 'Information', 'Home'];
 
     const ul = document.querySelector('ul')
 
     for (let navItem of navItems) {
         let li = document.createElement("li");
-        if (navItem == 'Section4') {
-            li.innerHTML += `<a class="navbara" href="#section4">${navItem}</a>`;
+        if (navItem == 'Information') {
+            li.innerHTML += `<a class="navbara" href="#information">${navItem}</a>`;
             li.id = navItem;
             li.className = "navbarli";
             li.style = "cover"
             ul.appendChild(li);
         } else if (navItem == 'Home') {
-            li.innerHTML += `<a class="navbara" href="#mainheader">${navItem}</a>`;
+            li.innerHTML += `<a class="navbara" href="#home">${navItem}</a>`;
             li.id = navItem;
             li.className = "navbarli";
             li.style = "float:right"
@@ -40,3 +40,105 @@ function createNav() {
 }
 
 createNav();
+
+/*
+Adding an EventListener to an Element getting it by ID and adding an active class to HTML .
+Making a function - reset() removing active class from all the elements leaving active class to 
+the element which is being clicked. 
+*/
+
+function reset() {
+    document.getElementById("news").classList.remove("your-active-class");
+    document.getElementById("life").classList.remove("your-active-class");
+    document.getElementById("comments").classList.remove("your-active-class");
+    document.getElementById("information").classList.remove("your-active-class");
+}
+document.getElementById("News").addEventListener("click", newsActive);
+
+function newsActive() {
+    reset();
+    document.getElementById("news").className += "your-active-class";
+
+}
+
+document.getElementById("Life").addEventListener("click", lifeActive);
+
+function lifeActive() {
+    reset();
+    document.getElementById("life").className += "your-active-class";
+
+}
+
+document.getElementById("Comments").addEventListener("click", commentsActive);
+
+function commentsActive() {
+    reset();
+    document.getElementById("comments").className += "your-active-class";
+
+}
+
+/* Used this code for making sections active with click.
+Added Navbar active on scroll to the following code where I make sections
+active in scroll.
+
+*/
+
+// document.getElementById("Information").addEventListener("click", informationActive);
+
+// function informationActive() {
+//     reset();
+//     document.getElementById("information").className += "your-active-class";
+
+// }
+
+// document.getElementById("home").addEventListener("click", homeDate);
+
+// function homeDate() {
+//     reset();
+//     document.getElementById("data").innerHTML = Date();
+
+// }
+
+// document.getElementById("Life").addEventListener("scroll", lifeScroll);
+
+// function lifeScroll() {
+
+//     document.getElementById("life").className += "your-active-class";
+
+// }
+
+/* 
+Got inspired by  Youtube video - Introduction to the Intersection Observer JavaScript API - 
+to make section active on scroll. Added Navbar active on scroll to same code. 
+*/
+// const sectionNews = document.querySelector('#news');
+const sections = document.querySelectorAll('section');
+const options = {
+    root: null,
+    threshhold: 0,
+    rootMargin: "-250px"
+};
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log(entry.target.id); //life, news
+            entry.target.classList.add('your-active-class');
+            let navid = entry.target.id //life
+                // make navid first char capital
+            navid = navid.charAt(0).toUpperCase() + navid.substring(1);
+            document.getElementById(navid).classList.add('navactive');
+        } else {
+            console.log(entry.target.id);
+            entry.target.classList.remove('your-active-class');
+            let navid = entry.target.id
+            navid = navid.charAt(0).toUpperCase() + navid.substring(1);
+            document.getElementById(navid).classList.remove('navactive');
+        }
+
+    });
+}, options);
+
+
+sections.forEach(section => {
+    observer.observe(section);
+});
